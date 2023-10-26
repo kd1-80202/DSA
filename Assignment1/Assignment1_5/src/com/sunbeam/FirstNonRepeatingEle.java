be95c7545f7c2c4eeaee24bff315d21162525718
74463Gauri <gaurimatkar58@gmail.com>
2023-10-26 23:39:34 +0530

    Assignment1

---
+++ Assignment1/Assignment1_5/src/com/sunbeam/FirstNonRepeatingEle.java
@@ -0,0 +1,31 @@
+package com.sunbeam;
+
+public class FirstNonRepeatingEle {
+	public static int getNonRepeatingElement(int arr[]) {
+    for(int i = 0; i<arr.length ; i++) {
+   	for(int j = 0 ; j<arr.length ; j++) {
+   		if(arr[i] == arr[j] && i!=j) 
+   			break;
+   		
+   		if(j==arr.length-1 )
+   			return arr[i];	
+   	}
+   	
+   			
+   	
+   	 
+    }
+	 return -1;
+	
+	
+}
+public static void main(String[] args) {
+	int arr[] = { 1, 2, 3, -1, 2, 1, 0, 4, -1, 7, 8 };
+   int element = getNonRepeatingElement(arr);
+   if(element != -1)
+   System.out.println("First non-repeating element is "+element);
+   else
+   	System.out.println("No such element exist!");
+}
+
+}
